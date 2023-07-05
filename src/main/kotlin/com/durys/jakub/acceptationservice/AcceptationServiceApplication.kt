119271b4c1c490c5ad928c5d032a759dc2756a4d
kubavation <37585627+kubavation@users.noreply.github.com>
2023-07-05 19:01:33 +0200

    init

---
+++ src/main/kotlin/com/durys/jakub/acceptationservice/AcceptationServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.durys.jakub.acceptationservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AcceptationServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<AcceptationServiceApplication>(*args)
+}
