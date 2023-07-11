1ba933e52eccc4b7758d5688065a91415e9428e4
kubavation <37585627+kubavation@users.noreply.github.com>
2023-07-11 20:10:09 +0200

    endpoint for acceptants

--- src/main/kotlin/com/durys/jakub/acceptationservice/acceptation/infrastructure/in/AcceptationController.kt
+++ src/main/kotlin/com/durys/jakub/acceptationservice/acceptation/infrastructure/in/AcceptationController.kt
@@ -1,12 +1,21 @@
 package com.durys.jakub.acceptationservice.acceptation.infrastructure.`in`
 
 import com.durys.jakub.acceptationservice.acceptation.domain.AcceptationRepository
+import com.durys.jakub.acceptationservice.acceptation.domain.Employee
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
 
 @RestController
 @RequestMapping("/api/acceptation")
 class AcceptationController(val acceptationRepository: AcceptationRepository) {
 
-    
+    @GetMapping("/employees/{employeeId}")
+    fun acceptants(@PathVariable employeeId: String,
+                   @RequestParam at: LocalDate, @RequestParam level: Int): List<Employee> {
+        return acceptationRepository.acceptants(employeeId, level, at)
+    }
 }
