db2478866e862416b8a486e6cd0b82bfa6a8d165
kubavation <37585627+kubavation@users.noreply.github.com>
2023-07-18 21:03:50 +0200

    endpoint for employee config

--- src/main/kotlin/com/durys/jakub/acceptationservice/acceptation/domain/AcceptationRepository.kt
+++ src/main/kotlin/com/durys/jakub/acceptationservice/acceptation/domain/AcceptationRepository.kt
@@ -10,4 +10,9 @@ interface AcceptationRepository: Neo4jRepository<Employee, String> {
             " where e.id = \$employeeId and supervised.level = \$level and (\$at >= supervised.from and \$at <= coalesce(supervised.to, \$at))" +
             " return supervisor")
     fun acceptants(employeeId: String, level: Int, at: LocalDate): List<Employee>
+
+    @Query("MATCH (e:Employee) <-[supervised:SUPERVISED_BY]-(supervisor:Employee)" +
+            " where e.id = \$employeeId and supervised.level = \$level" +
+            " return supervisor, supervised")
+    fun supervisorConfig(employeeId: String, level: Int): List<Employee>
 }
